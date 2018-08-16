5636891a928ca24efa671bd92429dca1c245adba
zlubsen <zeeger@lubsen.eu>
2018-08-16 13:23:43 +0200

    Split FrequencyTimer in separate h and cpp files
    Now linker does not complain anymore.

---
+++ lib/timers/src/FrequencyTimer.cpp
@@ -0,0 +1,25 @@
+#include "FrequencyTimer.h"
+
+FrequencyTimer::FrequencyTimer(int frequency_ms) {
+  setFrequency(frequency_ms);
+  setLastTime();
+}
+
+void FrequencyTimer::setLastTime() {
+  _last_time_ms = millis();
+}
+
+bool FrequencyTimer::elapsed() {
+  unsigned long current_time_ms = millis();
+
+  if(current_time_ms - _last_time_ms > _frequency_ms) {
+    setLastTime();
+    return true;
+  }
+
+  return false;
+}
+
+void FrequencyTimer::setFrequency(int frequency_ms) {
+  _frequency_ms = frequency_ms;
+}
---
+++ lib/timers/src/FrequencyTimer.h
@@ -0,0 +1,18 @@
+#ifndef Timers_FrequencyTimer_h
+#define Timers_FrequencyTimer_h
+
+#include <Arduino.h>
+
+class FrequencyTimer {
+  public:
+    FrequencyTimer(int frequency_ms);
+    bool elapsed();
+    void setFrequency(int frequency_ms);
+
+  private:
+    unsigned long _frequency_ms;
+    unsigned long _last_time_ms;
+    void setLastTime();
+};
+
+#endif
