a532981b4cd52cbbe3ba86966908e7ec196c123e
zlubsen <zeeger@lubsen.eu>
2018-08-20 14:41:23 +0200

    Change config

--- src/config/NovaConfig.cpp
+++ src/config/NovaConfig.cpp
@@ -2,7 +2,7 @@
 #include <PID_v1.h>
 
 NovaConfig::NovaConfig() {
-  _status_publish_frequency_ms = 1000;
+  _status_publish_frequency_ms = 2000;
 
   _joy_left_config.x = 0.05;
   _joy_left_config.y = 0.05;
