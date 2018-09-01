28586820b3d1382c2bca5aa405fd98b534bd9b72
Zeeger <zeeger@lubsen.eu>
2018-09-01 22:11:25 +0200

    Change access to vars and functions to accomodate subclassing JoystickControlLoop

--- src/controlloops/JoystickControlLoop.h
+++ src/controlloops/JoystickControlLoop.h
@@ -22,23 +22,25 @@ class JoystickControlLoop : public AbstractControlLoop {
     JoystickControlLoop(HardwareConfig *hardwareConfig, NovaConfig *novaConfig);
     void run(NovaCommand* cmd);
 
-  private:
-    Joystick *_joy_left;
-    Joystick *_joy_right;
+  protected:
     joystick_status _joy_left_input;
     joystick_status _joy_right_input;
     output_degrees _joy_left_output;
     output_degrees _joy_right_output;
-    filter_constant _filterconst_left;
-    filter_constant _filterconst_right;
     NovaServo *_servo1;
     NovaServo *_servo2;
     NovaServo *_servo3;
     NovaServo *_servo4;
     NovaServo *_servo5;
+    void actuateStepwiseInputServo(NovaServo* servo);
+    
+  private:
+    Joystick *_joy_left;
+    Joystick *_joy_right;
+    filter_constant _filterconst_left;
+    filter_constant _filterconst_right;
     const int _switch_degree_stepsize = 2; // TODO move to NovaConfig class
     void actuateContinousInputServo(NovaServo* servo, int degree);
-    void actuateStepwiseInputServo(NovaServo* servo);
     void mapInputToRange();
     void filterInput();
     void observe();
